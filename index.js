6c9118c39dfff71fdf52bafd68d8f0a5019ed222
gfyscdhuijk <crazybonepsg@hotmail.com>
2024-05-10 09:15:20 +0700

    add more features

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function reverseString(str) {
-  return str.split("").reverse().join("");
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
 }
