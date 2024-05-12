2ab97b83f6840248090434fd3c9d9c6ef007feac
gfyscdhuijk <crazybonepsg@hotmail.com>
2024-05-12 23:21:31 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-function exist(board, word) {
-  const rows = board.length;
-  const cols = board[0].length;
-  const dfs = (i, j, index) => {
-    if (index === word.length) return true;
-    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] !== word[index])
-      return false;
-    const temp = board[i][j];
-    board[i][j] = "";
-    const found =
-      dfs(i + 1, j, index + 1) ||
-      dfs(i - 1, j, index + 1) ||
-      dfs(i, j + 1, index + 1) ||
-      dfs(i, j - 1, index + 1);
-    board[i][j] = temp;
-    return found;
-  };
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (board[i][j] === word[0] && dfs(i, j, 0)) return true;
+function combinationSum2(candidates, target) {
+  candidates.sort((a, b) => a - b);
+  const result = [];
+  backtrack([], 0, 0);
+  return result;
+  function backtrack(combination, start, sum) {
+    if (sum === target) {
+      result.push([...combination]);
+      return;
+    }
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      if (i > start && candidates[i] === candidates[i - 1]) continue;
+      combination.push(candidates[i]);
+      backtrack(combination, i + 1, sum + candidates[i]);
+      combination.pop();
     }
   }
-  return false;
 }
