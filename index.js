9d1aa42bc50f40f89f109109819e638b0e5f9325
gfyscdhuijk <crazybonepsg@hotmail.com>
2024-05-14 18:44:45 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function jump(nums) {
-  let jumps = 0;
-  let currentEnd = 0;
-  let farthest = 0;
-  for (let i = 0; i < nums.length - 1; i++) {
-    farthest = Math.max(farthest, i + nums[i]);
-    if (i === currentEnd) {
-      jumps++;
-      currentEnd = farthest;
-    }
-  }
-  return jumps;
+function countWords(str) {
+  return str.split(" ").filter((word) => word !== "").length;
 }
